b964925ae134f0c613a74b0faa63cb71e45fbaca
Varsha Pandey <varsha280902@gmail.com>
2024-04-27 11:26:12 +0530

    complete update listing fuctionality

--- mern project/client/src/App.jsx
+++ mern project/client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import Welcome from "./pages/Welcome";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
 
 
 export default function App() {
@@ -22,6 +23,7 @@ export default function App() {
         <Route path='/profile' element={<Profile />} />
         <Route path='/welcome' element={<Welcome />}/>
         <Route path='/create-listing' element={<CreateListing />}/>
+        <Route path='/update-listing/:listingId' element={<UpdateListing />}/>
       </Route>
     </Routes>
   </BrowserRouter>
