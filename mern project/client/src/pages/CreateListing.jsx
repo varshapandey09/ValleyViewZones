8b6f4a9924eed8b1b5f85e937082ee246088a693
Varsha Pandey <varsha280902@gmail.com>
2024-05-22 15:50:41 +0530

    styling

--- mern project/client/src/pages/CreateListing.jsx
+++ mern project/client/src/pages/CreateListing.jsx
@@ -329,7 +329,7 @@ export default function CreateListing() {
               type='button'
               disabled={uploading}
               onClick={handleImageSubmit}
-              className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
+              className='p-3 bg-green-700 text-white rounded uppercase hover:shadow-lg disabled:opacity-80'
             >
               {uploading ? 'Uploading...' : 'Upload'}
             </button>
