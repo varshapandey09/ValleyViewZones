3fe37864038cd2c2f535349d29c5ebf5245136f9
H P <varsha280902@gmail.com>
2024-05-26 19:37:14 +0530

    about and privacy policy

--- mern project/client/src/pages/About.jsx
+++ mern project/client/src/pages/About.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export default function About() {
   return (
     <div style={{
-      backgroundImage: "url('https://cdn.create.vista.com/api/media/medium/352613388/stock-photo-home-love-warm-friendly-environment-wooden-housing-model-real-estate?token=')",
+      backgroundImage: "url('https://img.freepik.com/premium-photo/shower-curtain-isolated_719385-2471.jpg?w=740')",
       backgroundSize: 'cover', // Adjust as needed (cover, contain, etc.)
       backgroundPosition: 'center', // Adjust as needed (center, top left, etc.)
       height: '90vh', // Adjust height as needed
       width: '100vw', // Adjust width as needed
     }}>
     <div className='py-20 px-4 max-w-6xl mx-auto'>
-      <h1 className='text-xl sm:text-3xl font-bold mb-4 text-slate-900'>About ValleyViewZones</h1>
-      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-semibold'>ValleyViewZones is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
-      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-semibold'>
+      <h1 className='text-xl sm:text-3xl font-extrabold mb-4 text-slate-900'>About ValleyViewZones</h1>
+      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-bold'>ValleyViewZones is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-bold'>
       Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
       </p>
-      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-semibold'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className='text-sm sm:text-lg mb-4 text-gray-800 font-bold'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
     </div>
   )
--- mern project/client/src/pages/PrivacyPolicy.jsx
+++ mern project/client/src/pages/PrivacyPolicy.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 export default function PrivacyPolicy() {
   return (
+    <div style={{
+      backgroundImage: "url('https://img.freepik.com/free-photo/background-with-simple-white-wall_23-2151020040.jpg?size=626&ext=jpg&ga=GA1.1.1539263789.1716731795&semt=ais_user')",
+      backgroundSize: 'cover', // Adjust as needed (cover, contain, etc.)
+      backgroundPosition: 'center', // Adjust as needed (center, top left, etc.)
+      height: '90vh', // Adjust height as needed
+      width: '100vw', // Adjust width as needed
+    }}>
       <div className='py-20 px-4 max-w-6xl mx-auto'>
-        <h1 className='text-xl sm:text-3xl font-bold mb-4 text-slate-900'>ValleyViewZones Privacy Policy</h1>
-        <p className='text-sm sm:text-lg mb-4 text-gray-800 font-semibold'>
-        ValleyViewZones takes your privacy seriously. This Privacy Policy explains how we collect, use, and disclose your information when you use our web application. We collect information you provide directly (email, search criteria) and information collected automatically (device type, IP address, usage data). We use this information to operate the app, personalize your experience, connect you with relevant listings and agents and comply with legal requirements. You can access and update your information within the profile page. We may share information with third-party service providers who assist us and may disclose it to law enforcement if required. We will never sell or rent your personal information.
+        <h1 className='text-xl sm:text-3xl font-extrabold mb-4 text-slate-900'>ValleyViewZones Privacy Policy</h1>
+        <p className='text-sm sm:text-lg mb-4 text-gray-800 font-bold'>
+        ValleyViewZones takes your privacy seriously. 
+        </p>
+        <p className='text-sm sm:text-lg mb-4 text-gray-800 py-2 font-bold'>
+        This Privacy Policy explains how we collect, use, and disclose your information when you use our web application. We collect information you provide directly (email, search criteria) and information collected automatically (device type, IP address, usage data). 
+        </p>
+        <p className='text-sm sm:text-lg mb-4 text-gray-800 font-bold'>
+        We use this information to operate the app, personalize your experience, connect you with relevant listings and agents and comply with legal requirements. You can access and update your information within the profile page. We may share information with third-party service providers who assist us and may disclose it to law enforcement if required. We will never sell or rent your personal information.
         </p> 
     </div>
+  </div>
 )
 }
