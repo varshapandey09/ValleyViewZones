a8de328e7da1c490a655373b86caa41721a64836
H P <varsha280902@gmail.com>
2024-05-26 15:44:16 +0530

    making home page responsive

--- mern project/client/src/pages/Home.jsx
+++ mern project/client/src/pages/Home.jsx
@@ -83,21 +83,21 @@ export default function Home() {
           animate="visible"
           variants={textVariants}
         >
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-3xl sm:text-5xl font-bold">
             <span className='text-blue-900'>Valley</span>
             <span className='text-green-900'>View</span>
             <span className='text-violet-900'>Zones</span>
           </h1>
-          <h3 className="mt-4 text-3xl font-bold text-black">
+          <h3 className="text-xl sm:text-3xl mt-4 font-bold text-black">
             <big> Find your next <span className="text-blue-900">perfect</span> place with ease</big>
           </h3>
-          <div className="mt-2 text-lg">
+          <div className="text-sm sm:text-lg mt-2">
             <big><p className='text-black'>ValleyViewZones is the best place to find your next perfect place to live.</p></big>
             <big><p className='text-black'>We have a wide range of properties for you to choose from.</p></big>
           </div>
           <Link
             to={'/search'}
-            className="mt-4 text-lg text-blue-950 font-bold hover:underline px-4 py-2 rounded"
+            className="text-sm sm:text-lg mt-4 text-blue-950 font-bold hover:underline px-4 py-2 rounded"
           >
             Let's get started...
           </Link>
@@ -105,31 +105,31 @@ export default function Home() {
       </div>
 
       <div class="flex flex-row bg-white rounded-lg py-10">
-        <div class="text-container bg-white w-3/5 mx-auto py-10 pl-16 ">
-          <h2 class="text-4xl font-bold mb-10 text-blue-900">
+        <div class="text-container bg-white w-5/6 sm:w-3/5 mx-auto py-10 pl-0 sm:pl-16">
+          <h2 class="text-2xl sm:text-4xl font-bold mb-10 text-blue-900">
             Imagine your dream home...
           </h2>
-          <p class="text-lg mb-8 leading-loose text-slate-400">
+          <p class="text-sm sm:text-lg mb-8 leading-loose text-slate-400">
             Is it a place filled with laughter and cozy evenings by the fireplace? 
             Maybe it's a bright and airy space perfect for entertaining friends and family.
             Whatever your vision, <span className='text-blue-700 font-bold'>Valley</span><span className='text-green-700 font-bold'>View</span><span className='text-violet-700 font-bold'>Zones</span> helps you find the perfect match.
           </p>
           <ul class="list-none p-0 mb-8 pl-6">
             <li class="flex items-center space-x-4">
-              <span class="text-slate-500 text-lg font-bold leading-loose">Stop endless searching! Find your haven in minutes.</span>
+              <span class="text-slate-500 text-sm sm:text-lg font-bold leading-loose">Stop endless searching! Find your haven in minutes.</span>
             </li>
             <li class="flex items-center space-x-4">
-              <span class="text-slate-500 text-lg font-bold leading-loose">Fall in love with beautiful listings curated for you.</span>
+              <span class="text-slate-500 text-sm sm:text-lg font-bold leading-loose">Fall in love with beautiful listings curated for you.</span>
             </li>
             <li class="flex items-center space-x-4">
-              <span class="text-slate-500 text-lg font-bold leading-loose">Turn the key and unlock a new chapter filled with joy.</span>
+              <span class="text-slate-500 text-sm sm:text-lg font-bold leading-loose">Turn the key and unlock a new chapter filled with joy.</span>
             </li>
           </ul>
-          <p class="text-lg font-bold text-slate-400">
+          <p class="text-sm sm:text-lg font-bold text-slate-400">
             Let's make your dream home a  <span className='text-blue-900'> R E A L I T Y!</span>
           </p>
         </div>
-        <div className='pb-16'>
+        <div className='hidden sm:inline pb-16'>
           <img src="https://t3.ftcdn.net/jpg/01/67/07/12/360_F_167071270_P6D1HSu3HD9FYngLzF5bmCIJMgTDL32b.jpg"
           alt="Dream home image"
           class="object-cover h-full"
